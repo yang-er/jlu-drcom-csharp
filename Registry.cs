fc1a89d7b2ef9536b9dd7af8ab2197c90b56456a
小羊 <tlylz99@live.cn>
2018-01-28 15:20:53 +0800

    Small Fix

--- Registry.cs
+++ Registry.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using Microsoft.Win32;
+﻿using Microsoft.Win32;
 
 namespace DrClient
 {
 
-    public partial class MainForm : Form
+    public partial class MainForm
     {
 
         private void LoadMacFromRegistry()
